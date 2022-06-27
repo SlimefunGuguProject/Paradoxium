26119cccb77614d28f812957a1e3eb7ee7844bd0
LinYing <984672168@qq.com>
2022-06-28 03:10:34 +0800

    Update WarpConnections.java

--- src/main/java/com/ncoder/paradoxium/api/WarpConnections.java
+++ src/main/java/com/ncoder/paradoxium/api/WarpConnections.java
@@ -1,9 +1,9 @@
-package com.ncoder.paradoxium.api;
+package com.ncoder.paradox.api;
 
-import com.ncoder.paradoxium.ParadoxConfig;
-import com.ncoder.paradoxium.items.blocks.WarpPlate;
-import com.ncoder.paradoxium.utils.Constants;
-import com.ncoder.paradoxlib.utils.ConversionUtil;
+import com.ncoder.paradox.ParadoxConfig;
+import com.ncoder.paradox.items.blocks.WarpPlate;
+import com.ncoder.paradox.utils.Strings;
+import com.ncoder.paradox.utils.Utils;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
@@ -20,9 +20,8 @@ import java.util.List;
 import java.util.UUID;
 
 public class WarpConnections {
-
-    private final int[] BORDER = { 0, 1, 2, 3, 5, 6, 7, 8, 45, 47, 48, 49, 50, 51, 53 };
-    private final int[] INVENTORY = { 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44 };
+    private final int[] border = { 0, 1, 2, 3, 5, 6, 7, 8, 45, 47, 48, 49, 50, 51, 53 };
+    private final int[] inventory = { 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44 };
 
     private final int CANCEL = 4;
     private final int PREVIOUS = 46;
@@ -31,13 +30,13 @@ public class WarpConnections {
     private int page = 1;
 
     public void openPlateSelector(Player p, UUID uuid, Block b, BlockMenuPreset preset, BlockMenu bm) {
-        ChestMenu menu = new ChestMenu(ChatColor.GOLD + "选择板材");
+        ChestMenu menu = new ChestMenu(ChatColor.GOLD + "Select Plates");
 
-        for (int i : BORDER) {
-            menu.addItem(i, new CustomItemStack(Material.GRAY_STAINED_GLASS_PANE, Constants.EMPTY), ChestMenuUtils.getEmptyClickHandler());
+        for (int i : border) {
+            menu.addItem(i, new CustomItemStack(Material.GRAY_STAINED_GLASS_PANE, Strings.EMPTY), ChestMenuUtils.getEmptyClickHandler());
         }
 
-        menu.addItem(CANCEL, new CustomItemStack(Material.BARRIER, "&4取消"));
+        menu.addItem(CANCEL, new CustomItemStack(Material.BARRIER, "&4Cancel"));
         menu.addMenuClickHandler(CANCEL, (pl, slot, item, action) -> {
             pl.closeInventory();
             preset.newInstance(bm, b);
@@ -50,12 +49,12 @@ public class WarpConnections {
         createPage(uuid, menu, b, preset, bm);
 
         if (amount == 1) {
-            menu.addItem(PREVIOUS, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&f◄ &6上一页", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
-            menu.addItem(NEXT, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&6下一页 &f►", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
+            menu.addItem(PREVIOUS, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&f◄ &6Previous", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
+            menu.addItem(NEXT, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&6Next &f►", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
         } else {
-            if (page <= 1) menu.addItem(PREVIOUS, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&f◄ &6上一页", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
+            if (page <= 1) menu.addItem(PREVIOUS, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&f◄ &6Previous", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
             else {
-                menu.addItem(PREVIOUS, new CustomItemStack(Material.LIME_STAINED_GLASS_PANE, "&f◄ &6上一页", "("+page+"/"+amount+")"));
+                menu.addItem(PREVIOUS, new CustomItemStack(Material.LIME_STAINED_GLASS_PANE, "&f◄ &6Previous", "("+page+"/"+amount+")"));
                 menu.addMenuClickHandler(PREVIOUS, (pl, slot, item, action) -> {
                     page -= 1;
                     createPage(uuid, menu, b, preset, bm);
@@ -63,9 +62,9 @@ public class WarpConnections {
                 });
             }
 
-            if (page >= amount) menu.addItem(NEXT, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&6下一页 &f►", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
+            if (page >= amount) menu.addItem(NEXT, new CustomItemStack(Material.BLACK_STAINED_GLASS_PANE, "&6Next &f►", "("+page+"/"+amount+")"), ChestMenuUtils.getEmptyClickHandler());
             else {
-                menu.addItem(NEXT, new CustomItemStack(Material.LIME_STAINED_GLASS_PANE, "&6下一页 &f►", "("+page+"/"+amount+")"));
+                menu.addItem(NEXT, new CustomItemStack(Material.LIME_STAINED_GLASS_PANE, "&6Next &f►", "("+page+"/"+amount+")"));
                 menu.addMenuClickHandler(NEXT, (pl, slot, item, action) -> {
                     page += 1;
                     createPage(uuid, menu, b, preset, bm);
@@ -82,16 +81,16 @@ public class WarpConnections {
 
         if (ParadoxConfig.WARPS.contains(uuid + ".warps." + l)) {
             List<String> plates = ParadoxConfig.WARPS.getStringList(uuid + ".warps." + l);
-            int counter = (page - 1) * INVENTORY.length;
-            for (int i : INVENTORY) {
+            int counter = (page - 1) * inventory.length;
+            for (int i : inventory) {
                 try {
                     final String plate = plates.get(counter);
-                    Location loc = ConversionUtil.LOCATION.toLocation(plate);
+                    Location loc = Utils.StringLocation(plate);
 
                     String name = BlockStorage.getLocationInfo(loc, "name");
                     Material material = WarpPlate.materials[Integer.parseInt(BlockStorage.getLocationInfo(loc, "material"))];
 
-                    menu.addItem(i, new CustomItemStack(material, !name.equals("") ? name : Constants.EMPTY, "&b世界&8: &7" + loc.getWorld().getName(), "&b坐标&8: &7" + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ()));
+                    menu.addItem(i, new CustomItemStack(material, !name.equals("") ? name : Strings.EMPTY, "&bWorld&8: &7" + loc.getWorld().getName(), "&bCoordinates&8: &7" + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ()));
                     menu.addMenuClickHandler(i, (pl, s, item, action) -> {
                         pl.closeInventory();
                         BlockStorage.addBlockInfo(b, "destination", plate);
@@ -105,7 +104,7 @@ public class WarpConnections {
                 counter++;
             }
         } else {
-            for (int i : INVENTORY) {
+            for (int i : inventory) {
                 menu.addMenuClickHandler(i, ChestMenuUtils.getEmptyClickHandler());
             }
         }
@@ -117,10 +116,9 @@ public class WarpConnections {
 
         if (ParadoxConfig.WARPS.contains(uuid + ".warps." + l)) {
             List<String> plates = ParadoxConfig.WARPS.getStringList(uuid + ".warps." + l);
-            amount += plates.size() / INVENTORY.length;
+            amount += plates.size() / inventory.length;
         }
 
         return amount;
     }
-
 }
